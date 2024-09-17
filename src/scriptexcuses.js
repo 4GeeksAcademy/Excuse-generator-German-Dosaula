0742a91ded43e5290306b711a9b62a1aa4bfa62e
germandosaula <german.dosaula@gmail.com>
2024-09-17 14:40:47 +0000

    Ejercicio Terminado

---
+++ src/scriptexcuses.js
@@ -0,0 +1,12 @@
+let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+let action = ["ate ", "peed ", "crushed ", "broke "];
+let what = ["my homework ", "my phone ", "the car "];
+let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"];
+
+let randomnumber1 = Math.floor(Math.random() * who.length);
+let randomnumber2 = Math.floor(Math.random() * action.length);
+let randomnumber3 = Math.floor(Math.random() * what.length);
+let randomnumber4 = Math.floor(Math.random() * when.length);
+
+document.querySelector("#excuse").innerHTML = who[randomnumber1] + action[randomnumber2] + what[randomnumber3] + when[randomnumber4];
+
